753848dd64e414442fe74d2f4c03f4f2b15ac3e8
clara-silva-martins <clara.dsm@gmail.com>
2025-03-02 18:32:29 -0300

    componente de contato e funções em typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,52 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { SeparadorComponent } from "./componentes/separador/separador.component";
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato{
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContainerComponent, CabecalhoComponent],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'indexa';
+
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = ''
+
+  filtrarContatosPorTexto (): Contato[] {
+    if(!this.filtroPorTexto){
+      return this.contatos
+    }
+    return this.contatos.filter(contato =>{
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
+    })
+  }
+
+  filtrarContatosPorLetraInicial(letra:string) : Contato[]{
+    return this.filtrarContatosPorTexto().filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    })
+  }
+
 }
